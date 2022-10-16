e9deb529df49a39df9cdde6d030fe7faaebb62ce
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-10-15 21:38:14 -0500

    first commit

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,22 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plantLists}) {
+  const renderLists = plantLists.map((list) => {
+    return (
+      <PlantCard 
+        key={list.id}
+        lists={list}
+      />
+    )});
+
+
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {/* render PlantCards components in here */}
+      {renderLists}
+    </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plantLists, setPlantLists] = useState([])
+  const [search, setSearch] = useState('')
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(r => r.json())
+    .then(plantArr => {
+      console.log(plantArr)
+      setPlantLists(plantArr)
+    })
+  }, [])
+
+  const onSearch = (searchPlant) => {
+    console.log(searchPlant);
+    setSearch(searchPlant);
+  };
+  const addPlant = (plantObj) => {
+    setPlantLists(plant => [plantObj, ...plant])
+  }
+
+  const filteredPlants = plantLists.filter((list) => {
+    return list.name.toLowerCase().includes(search.toLowerCase());
+  });
+
+  console.log(filteredPlants);
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={addPlant} />
+      <Search onSearch={onSearch} />
+      <PlantList plantLists={filteredPlants} />
     </main>
   );
 }
